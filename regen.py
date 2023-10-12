5e988ce8dbc4496ff52b49ddf4da0b2d47fa6253
Abir <ahammed.abir47@gmail.com>
2023-10-12 23:42:02 +0600

    Updated seemless download

--- regen.py
+++ regen.py
@@ -1,9 +1,14 @@
 import requests, xmltodict,icecream as ic
-import re
+import re, os
+import argparse
+import aiohttp
+import asyncio
+from tqdm import tqdm
+from tqdm.asyncio import tqdm_asyncio
 
-def recursive_call(url):
+async def recursive_call(url, home_path):
     if ('.' in url[-7:]):
-        generate_download_data(url)
+        await generate_download_data(url, home_path)
         return
     
     x = requests.get(url)
@@ -25,10 +30,53 @@ def recursive_call(url):
             match = re.search(href_regex, table_data[1])
             href_value = match.group(1)
             if href_value != "Games/" or href_value != "Software/":
-                recursive_call(url+href_value)
+                await recursive_call(url+href_value, home_path)
 
-def generate_download_data(value):
-    print(value)
+async def generate_download_data(url, home_path):
+    file_path = get_file_path(url)
+    full_file_path = home_path+file_path
+    await download_file(url, full_file_path)
 
-url = "http://10.16.100.206/ftps3/"
-recursive_call(url)
+def get_file_path(url):
+    path_array = url.replace("%20", " ").split('/')[3:-1]
+    return os.path.join(*path_array)
+
+
+
+# async def download_file(url, destination_path):
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url) as response:
+#             content = await response.read()
+#             with open(destination_path, "wb") as f:
+#                 f.write(content)
+
+
+async def download_file(url, destination_path):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.read()
+
+            # Create a tqdm progress bar
+            progress_bar = tqdm_asyncio.tqdm(total=len(content), desc="Downloading file")
+
+            # Write the content to the destination file while updating the progress bar
+            with open(destination_path, "wb") as f:
+                for chunk in progress_bar.iterable(content):
+                    f.write(chunk)
+
+async def main():
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('url')           # positional argument
+    parser.add_argument('-d', '--dest')      # option that takes a value
+    args = parser.parse_args()
+
+
+    print(f"Processing file: {args}")
+
+    # url = "http://10.16.100.206/ftps3/"
+    await recursive_call(args.url, args.dest)
+
+if __name__ == "__main__":
+    asyncio.run(main())
